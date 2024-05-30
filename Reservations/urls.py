37d5e8704384d98b4d0de5a887afc4bb76e268b5
Fernanda Valdivia <fernandavaldiviam09@gmail.com>
2024-05-30 17:19:50 -0600

    final version

--- Reservations/urls.py
+++ Reservations/urls.py
@@ -5,7 +5,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('token', obtain_auth_token),
+    path('token', obtain_auth_token),       #SUPERUSER: Fernanda PASSWORD: 123
     path('menu/', views.MenuItemView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
